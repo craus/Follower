09aae51ede040e31369dc4cc926e9d8d9d8447d7
craus <craus902@gmail.com>
2017-01-01 19:14:08 +0400

    follower - initial commit

---
+++ Assets/Checker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Mathematics;
+using SparseCollections;
+using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Checker : MonoBehaviour {
+
+    public Follower follower;
+    public Transform target;
+    public Slider slider;
+    public float baseDistance = 8;
+    public float speed = 0.1f;
+    public int cnt = 4;
+
+    public void Update() {
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.LogFormat("mouse = {0}", mouse);
+        float distance = (mouse - target.position.xy()).sqrMagnitude;
+        float quality = Mathf.Pow(0.5f, distance / baseDistance);
+        slider.value += (baseDistance - distance) * Time.deltaTime * speed;
+
+        if (slider.value == 1) {
+            NextLevel();
+            slider.value = 0;
+        }
+    }
+
+    void Start() {
+        NextLevel();
+    }
+
+    private void NextLevel() {
+        follower.RandomTrajectory(cnt);
+    }
+
+}
---
+++ Assets/Follower.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Mathematics;
+using SparseCollections;
+using System;
+using System.Linq;
+
+public class Follower : MonoBehaviour {
+
+    public TrajectoryPoint trajectoryPointPrefab;
+    float radius = 0.5f;
+    public Transform trajectoryTransform;
+    public RectTransform trajectoryRect;
+    public List<TrajectoryPoint> trajectory;
+    public float time;
+    public float period;
+    public float speed;
+
+    public SparseArray<int, double> xApproximation, yApproximation;
+
+    public float ApproximationFunction(float t, int id, bool y) {
+        //t = Extensions.Modulo(t, period);
+        
+        //return Mathf.Sin(2 * Mathf.PI * (id * 2 - 1) * t / period + id);
+
+        if (id == 0) {
+            return 1;
+        }
+        if (id % 2 == 0) {
+            return Mathf.Sin(Mathf.PI * 2 * ((1 + (id - 1) / 2) * t / period));
+        } else {
+            return Mathf.Cos(Mathf.PI * 2 * ((1 + (id - 1) / 2) * t / period));
+        }
+        //return Mathf.Cos(Mathf.PI * 2 * (t / period * id));
+    }
+
+    public Vector2 Position(float t) {
+        Vector2 result = Vector2.zero;
+        for (int i = 0; i < trajectory.Count; i++) {
+            result += new Vector2((float)(ApproximationFunction(t*speed, i, false) * xApproximation[i]), (float)(ApproximationFunction(t*speed, i, true) * yApproximation[i]));
+        }
+        return result;
+    }
+
+    public Vector2 SpookyPosition() {
+        float[] prefixProducts = new float[trajectory.Count];
+        prefixProducts[0] = Extensions.CyclicDiff(time, trajectory[0].time, period);
+        for (int i = 1; i < trajectory.Count; i++) {
+            prefixProducts[i] = prefixProducts[i - 1] * Extensions.CyclicDiff(time, trajectory[i].time, period);
+        }
+
+        float[] suffixProducts = new float[trajectory.Count];
+        suffixProducts[trajectory.Count - 1] = Extensions.CyclicDiff(time, trajectory[trajectory.Count - 1].time, period);
+        for (int i = trajectory.Count-2; i >= 0; i--) {
+            suffixProducts[i] = suffixProducts[i + 1] * Extensions.CyclicDiff(time, trajectory[i].time, period);
+        }
+
+        float totalWeight = 0;
+        Vector3 totalPosition = Vector2.zero;
+        for (int i = 0; i < trajectory.Count; i++) {
+            float weight = 1;
+            if (i > 0) {
+                weight *= prefixProducts[i-1];
+            }
+            if (i < trajectory.Count-1) {
+                weight *= suffixProducts[i+1];
+            }
+            totalPosition += weight * trajectory[i].transform.position;
+            totalWeight += weight;
+        }
+        return totalPosition / totalWeight;
+    }
+
+    void Update() {
+        time = Time.time;
+        RecalculateTrajectory();
+        RecalculatePosition();
+	}
+
+    void Start() {
+    }
+
+    [ContextMenu("Recalculate Position")]
+    void RecalculatePosition() {
+        transform.position = Position(time);
+    }
+
+    [ContextMenu("Recalculate Trajectory")]
+    void RecalculateTrajectory() {
+        trajectory = trajectoryTransform.GetComponentsInChildren<TrajectoryPoint>().ToList();
+        xApproximation = new SparseArray<int, double>();
+        yApproximation = new SparseArray<int, double>();
+        var mx = new SparseCollections.Sparse2DMatrix<int, int, double>();
+        var my = new SparseCollections.Sparse2DMatrix<int, int, double>();
+        var xa = new SparseCollections.SparseArray<int, double>();
+        var ya = new SparseCollections.SparseArray<int, double>();
+        for (int i = 0; i < trajectory.Count; i++) {
+            for (int j = 0; j < trajectory.Count; j++) {
+                mx[i, j] = ApproximationFunction(trajectory[i].time, j, false);
+                my[i, j] = ApproximationFunction(trajectory[i].time, j, true);
+            }
+            xa[i] = trajectory[i].transform.position.x;
+            ya[i] = trajectory[i].transform.position.y;
+        }
+
+        LinearEquationSolver.Solve(trajectory.Count, mx, xa, xApproximation);
+        LinearEquationSolver.Solve(trajectory.Count, my, ya, yApproximation);
+
+        //for (int i = 0; i < trajectory.Count; i++) {
+        //    Debug.LogFormat("xApproximation[{0}] = {1}", i, xApproximation[i]);
+        //}
+        //for (int i = 0; i < trajectory.Count; i++) {
+        //    Debug.LogFormat("yApproximation[{0}] = {1}", i, yApproximation[i]);
+        //}
+    }
+
+    [ContextMenu("Test")]
+    void Test() {
+        var mx = new SparseCollections.Sparse2DMatrix<int,int,double>();
+        mx[0,0] = 2;
+        mx[0,1] = 1;
+        mx[1,0] = 6;
+        mx[1,1] = -1;
+        var a = new SparseCollections.SparseArray<int, double>();
+        a[0] = 11;
+        a[1] = 13;
+        var x = new SparseCollections.SparseArray<int, double>();
+        LinearEquationSolver.Solve(2, mx, a, x);
+        Debug.LogFormat("x[0] = {0}", x[0]);
+        Debug.LogFormat("x[1] = {0}", x[1]);
+    }
+
+    [ContextMenu("Fit to screen")]
+    void FitToScreen() {
+        RecalculateTrajectory();
+        Debug.LogFormat("trajectory = {0}", trajectory.ExtToString());
+        Vector2 minPosition = Position(0);
+        Vector2 maxPosition = Position(0);
+        for (int i = 0; i < 1000; i++) {
+            Vector2 position = Position(period * i / 1000);
+            minPosition = Vector2.Min(minPosition, position);
+            maxPosition = Vector2.Max(maxPosition, position);
+        }
+        var corners = new Vector3[4];
+        Vector2 a = Camera.main.ScreenToWorldPoint(trajectoryRect.TransformPoint(trajectoryRect.rect.min));
+        Vector2 b = Camera.main.ScreenToWorldPoint(trajectoryRect.TransformPoint(trajectoryRect.rect.max));
+        Debug.LogFormat("corners: {0}, {1}", a, b);
+        Vector2 desiredMinPosition = Vector2.Min(a, b);
+        Vector2 desiredMaxPosition = Vector2.Max(a, b);
+        desiredMinPosition += Vector2.one * radius;
+        desiredMaxPosition -= Vector2.one * radius;
+        Vector2 zoom = (desiredMaxPosition - desiredMinPosition).Scaled((maxPosition - minPosition).Inverse());
+        zoom = Vector2.one * Mathf.Min(zoom.x, zoom.y);
+        Vector2 shift = (desiredMinPosition+desiredMaxPosition) - (minPosition+maxPosition).Scaled(zoom);
+        shift /= 2;
+        Debug.LogFormat("zoom = {0}, shift = {1}", zoom.ExtToString(), shift.ExtToString());
+        trajectory.ForEach(tp => tp.transform.position = tp.transform.position.xy().Scaled(zoom) + shift);
+        RecalculateTrajectory();
+    }
+
+    [ContextMenu("Random trajectory")]
+    void RandomTrajectory7() {
+        RandomTrajectory(7);
+    }
+
+    public void RandomTrajectory(int cnt) {
+        period = cnt;
+        RecalculateTrajectory();
+        trajectoryTransform.Children().ForEach(c => c.Destroy());
+        for (int i = 0; i < cnt; i++) {
+            var tp = Instantiate(trajectoryPointPrefab);
+            tp.transform.SetParent(trajectoryTransform);
+            tp.transform.position = UnityEngine.Random.insideUnitCircle * 100;
+            tp.time = UnityEngine.Random.Range(0, period);
+            //tp.transform.position = new Vector2(Mathf.Cos(Mathf.PI * 2 * i / cnt), Mathf.Sin(Mathf.PI * 2 * i / cnt));
+            tp.time = period * i / cnt + period / cnt * 0.2f * UnityEngine.Random.Range(-1f, 1f);
+        }
+        FitToScreen();
+    }
+}
